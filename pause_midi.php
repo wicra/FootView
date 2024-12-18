adf7d89d13e64cb7ae55b7ba1f88e773329a576b
Sergio Wicramachine <127046969+wicra@users.noreply.github.com>
2024-12-18 17:18:25 +0100

    pause midi

--- pause_midi.php
+++ pause_midi.php
@@ -18,6 +18,25 @@
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet">
     
     <style>
+        /* Styles spécifiques à la page */
+        path {
+            transform-origin: 50% 0%;
+        }
+        
+        .background--custom {
+            background-color: #d63a3a;
+            position: absolute;
+            width: 100vw;
+            height: 100vh;
+            z-index: -1;
+            top: 0;
+            left: 0;
+        }
+        
+        @keyframes path0 {
+            0% { transform: rotate(40deg); }
+            100% { transform: rotate(-40deg); }
+        }
         * {
             margin: 0;
             padding: 0;
@@ -27,9 +46,7 @@
         body {
             font-family: "Roboto", sans-serif;
             min-height: 100vh;
-            background: linear-gradient(135deg, #1a2a6c, #b21f1f, #fdbb2d);
             background-size: 400% 400%;
-            animation: gradient 15s ease infinite;
             display: flex;
             justify-content: center;
             align-items: center;
@@ -173,18 +190,30 @@
 
         .food {
             position: absolute;
-            width: 40%;
-            height: 12px;
-            background: linear-gradient(to bottom, rgb(82, 33, 33), rgb(200, 106, 106));
+            width: 100px;
+            height: 100px;
             left: 20px;
-            border-radius: 50%;
-            animation: flip 1.7s infinite;
+            top: -58px;
             z-index: 2;
+            animation: flip 1.7s infinite;
         }
+
+        .food svg {
+            width: 100%;
+            height: 100%;
+            filter: drop-shadow(0 0 5px rgba(0,0,0,0.3));
+        }
+
         @keyframes flip {
-            0% { transform: translateY(0px) rotate(0deg); }
-            50% { transform: translateY(-200px) rotate(180deg); }
-            100% { transform: translateY(0px) rotate(360deg); }
+            0% { 
+                transform: translateY(0px) rotate(0deg); 
+            }
+            50% { 
+                transform: translateY(-150px) rotate(720deg);
+            }
+            100% { 
+                transform: translateY(0px) rotate(0deg);
+            }
         }
         .panBase {
             z-index: 3;
@@ -217,6 +246,16 @@
     </style>
 </head>
 <body>
+
+    <!-- ARRIÈRE-PLAN PERSONNALISÉ -->
+    <svg class="background--custom" id="demo" viewBox="0 0 100 100" preserveAspectRatio="none">
+        <path 
+            fill="#4F4FF0"
+            fill-opacity="0.9"
+            d="M-100 -100L200 -100L200 50L-100 50Z"
+            style="animation: path0 12.195121951219512s linear infinite alternate"
+        ></path>
+    </svg>
     <div class="container">
         <div class="pause-header">
             <div class="pause-subtitle">Pause Midi</div>
@@ -228,7 +267,14 @@
                 <div class="pause-loader">
                     <div class="panWrapper">
                         <div class="pan">
-                            <div class="food"></div>
+                            <div class="food">
+                                <svg fill="#FFFFFF" width="800px" height="800px" viewBox="0 0 122.88 122.88" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="enable-background:new 0 0 122.88 122.88" xml:space="preserve">
+                                    <style type="text/css">.st0{fill-rule:evenodd;clip-rule:evenodd;}</style>
+                                    <g>
+                                        <path class="st0" d="M61.44,0c16.97,0,32.33,6.88,43.44,18c11.12,11.12,18,26.48,18,43.44c0,16.97-6.88,32.33-18,43.44 c-11.12,11.12-26.48,18-43.44,18S29.11,116,18,104.88C6.88,93.77,0,78.41,0,61.44C0,44.47,6.88,29.11,18,18 C29.11,6.88,44.47,0,61.44,0L61.44,0z M76.85,117.08L76.73,117l6.89-23.09L69.41,78.15L52.66,78L39.38,94.62l6.66,22.32l-0.15,0.1 c4.95,1.38,10.16,2.12,15.55,2.12C66.78,119.16,71.95,118.44,76.85,117.08L76.85,117.08z M12.22,91.61l24.34,0.12L49.28,75.8 l-5.26-16.12l-21.42-9.3L3.78,64.08C4.23,74.14,7.26,83.53,12.22,91.61L12.22,91.61z M16.77,24.88l7.4,22.14l19.98,8.68 l15.44-11.97V20.94L40.51,7.63c-7.52,2.93-14.28,7.39-19.89,13C19.27,21.98,17.98,23.4,16.77,24.88L16.77,24.88z M81.7,7.37 L63.3,20.77V43.7L77.8,54.91l20.81-8.92l7.18-21.49c-1.12-1.35-2.3-2.64-3.54-3.88C96.48,14.85,89.49,10.29,81.7,7.37L81.7,7.37z M119.09,64.36l-0.02,0.01L99.09,49.82l-19.81,8.49l-6.08,18.03l13.73,15.23c0.06,0.06,0.09,0.13,0.11,0.21l23.6-0.11 C115.56,83.65,118.59,74.34,119.09,64.36L119.09,64.36z"/>
+                                    </g>
+                                </svg>
+                            </div>
                             <div class="panBase"></div>
                             <div class="panHandle"></div>
                         </div>
